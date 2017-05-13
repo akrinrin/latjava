8d4dca62c88a77159275288c4174901b8bf4d788
akrinrin <syakirinamin11@gmail.com>
2017-05-13 20:59:40 +0700

    hitung.java added

---
+++ Hitung.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+
+public class Hitung
+{
+	public static void hitung(String[] args)
+	
+	{
+			
+			
+			int score = 0;
+			int benar=0;
+			int salah=0;
+			
+			Scanner scan = new Scanner(System.in);
+		
+			System.out.println("Terdapat 20 soal matematika\ntentang penjumlahan\njika nilai benar anda mendapat score 1\njika salah score anda dikurangi 1\n\n");
+			for (int i=1;i<=20;i++)
+			{
+				Random r = new Random();
+				int a = 1+r.nextInt(20);
+				int b = 1+r.nextInt(30);
+				
+				int pertanyaan = a+b;
+				System.out.print(i+". hasil dari "+a+"+"+b+"= ");
+				
+				int jawaban = scan.nextInt();
+				
+			
+			if (pertanyaan==jawaban)			
+			 {
+			 score++;
+			 benar++;	 
+			}
+			else
+			{
+				if (score==0)
+					score=0;
+				else
+					score--;
+			  		salah++;		 	 
+			}
+		}
+			System.out.println("\nBenar: "+benar);
+			System.out.println("Salah: "+salah);
+			System.out.println("total score: "+score);
+	}
+}
